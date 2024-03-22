9cdd5f46fc8b05479e1d2b1bec57df3b65359661
NeeRaj556 <niraj.bj85@gmail.com>
2024-03-22 23:12:21 +0545

    the booking form subbmitted

--- src/Admin dasbord/components/Khalti/khalti.jsx
+++ src/Admin dasbord/components/Khalti/khalti.jsx
@@ -4,7 +4,7 @@ import config from "./khaltiConfig";
 
 export default function Khalti(props) {
 
-  const { amount } = props; // Destructure the value prop
+  const { value } = props; // Destructure the value prop
 
 
   let checkout = new KhaltiCheckout(config);
@@ -12,11 +12,7 @@ export default function Khalti(props) {
     <div>
       <button
       
-        onClick={(event) => {
-          event.preventDefault(); 
-          checkout.show({ amount: props.amount })
-        }
-      }
+        onClick={() => checkout.show({ amount: value })}
         className=" bg-primary-7 hover:bg-white hover:text-primary-7 text-white transition-all duration-700  py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       >
         Pay Via Khalti
