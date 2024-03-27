aebd35db175c898a7e8c92aaf1339f95dba64cb5
NeeRaj556 <niraj.bj85@gmail.com>
2024-03-27 19:44:23 +0545

    the api has been fixed:

--- src/Config/config.jsx
+++ src/Config/config.jsx
@@ -1,4 +1,4 @@
 const webApi = {
-    apiUrl : "https://jolly-elegant-heron.ngrok-free.app"
+    apiUrl : "https://8aa4-2400-1a00-b060-2cf7-88c0-5b63-a5f7-6110.ngrok-free.app"
 }
 export default webApi;
